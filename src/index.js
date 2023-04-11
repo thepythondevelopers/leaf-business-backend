9fb1ddb8e7af6e99890ea5be9adb59c268a4b1b2
vishal750846 <vishal21071995@gmail.com>
2023-04-11 18:38:43 +0530

    'third'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
 const express = require('express')
-
 const cors = require("cors")
 require('./db/mongoose')
 const app = express()
 const userRoutes = require('./routes/user')
+
+const env = process.env.ENVIRONMENT;
+const cert = process.env.SSL_CERT;
+const priv_key = process.env.SSL_PRIV_KEY;
+let port = process.env.LOCAL_PORT;
+
+if (env == "PROD") {
+    port = process.env.PROD_PORT;
+  }
 
 app.use(express.json())
 app.use(cors())
@@ -11,7 +20,9 @@ app.use(userRoutes)
 
 
 
-app.listen(8000,()=>{
-    console.log('working on port 8000')
+
+
+app.listen(port,()=>{
+    console.log('working on port'+ port)
 })
 
